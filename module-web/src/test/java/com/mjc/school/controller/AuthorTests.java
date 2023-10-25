8906aa78f7846e66b653d86542c6e3b5fba71078
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-10-25 20:10:49 +0200

    add more controller tests

--- module-web/src/test/java/com/mjc/school/controller/AuthorTests.java
+++ module-web/src/test/java/com/mjc/school/controller/AuthorTests.java
@@ -3,62 +3,84 @@ package com.mjc.school.controller;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import static com.mjc.school.controller.TestHelper.*;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 
 public class AuthorTests {
-    private final String BASE = "http://localhost:8080/";
 
     @Test
     public void createAuthorTest() {
-        String authorName = "testAuthor";
-
-        JSONObject reqBody = new JSONObject();
-        reqBody.put("name", authorName);
-
-        given().contentType("application/json").body(reqBody.toString())
-                .when().post(BASE + "authors/create")
-                .then()
-                .statusCode(200)
-                .body("name", equalTo(authorName))
-                .body("id", notNullValue());
+        createAuthor(DEFAULT_AUTHOR_NAME);
     }
 
     @Test
     public void readAuthorByIdTest() {
+        var id = createAuthor(DEFAULT_AUTHOR_NAME);
+
+        var reqBody = new JSONObject();
+        reqBody.put("id", id);
+
+        given().contentType("application/json").body(reqBody.toString())
+                .when().get(AUTHORS + id)
+                .then()
+                .statusCode(200)
+                .body("name", equalTo(DEFAULT_AUTHOR_NAME))
+                .body("id", equalTo(id));
 
     }
 
     @Test
     public void readAllAuthorsTest() {
+        createAuthor(DEFAULT_AUTHOR_NAME);
+        createAuthor(DEFAULT_AUTHOR_NAME);
 
+        given().contentType("application/json")
+                .when().get(AUTHORS)
+                .then()
+                .statusCode(200)
+                .body("", hasSize(greaterThanOrEqualTo(2)));
     }
 
+    @Test
     public void updateAuthorTest() {
+        String authorNameUpdated = DEFAULT_AUTHOR_NAME + "Updated";
+
+        var id = createAuthor(DEFAULT_AUTHOR_NAME);
+
+        var reqBody = new JSONObject();
+        reqBody.put("name", authorNameUpdated);
+        reqBody.put("id", id);
+
+        given().contentType("application/json").body(reqBody.toString())
+                .when().patch(AUTHORS + id + "/update")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo(authorNameUpdated));
 
     }
 
     @Test
+    public void updateAuthorTestInvalid() {
+        var id = createAuthor(DEFAULT_AUTHOR_NAME);
+
+        var reqBodyInvalid = new JSONObject();
+        reqBodyInvalid.put("name", "a");
+        reqBodyInvalid.put("id", id);
+
+        given().contentType("application/json").body(reqBodyInvalid.toString())
+                .when().patch(AUTHORS + id + "/update")
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
     public void deleteAuthorTest() {
-        String authorName = "testAuthor";
-
-        JSONObject createReq = new JSONObject();
-        createReq.put("name", authorName);
-
-        var authorId = given().contentType("application/json").body(createReq.toString())
-                .when().post(BASE + "authors/create")
-                .then()
-                .statusCode(200)
-                .body("name", equalTo(authorName))
-                .body("id", notNullValue())
-                .extract()
-                .path("id");
-        
+        var id = createAuthor(DEFAULT_AUTHOR_NAME);
         given()
-                .when().delete(BASE + "authors/" + authorId +"/delete")
+                .when().delete(AUTHORS + id +"/delete")
                 .then()
-                .statusCode(200);
+                .statusCode(404);
     }
 }
